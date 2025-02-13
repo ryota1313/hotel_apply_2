5cc52580ea338d2a2fc69f79a03896bcb8df60b4
ryota1313 <gbale.jwil.cfab@gmail.com>
2025-02-13 18:01:44 +0900

    Planコントローラーの作成

---
+++ resources/views/top.blade.php
@@ -0,0 +1,5 @@
+<h1>トップページ</h1>
+<a href="#">部屋情報</a><br>
+<a href="#">空室カレンダー</a><br>
+<a href="{{ route('plan.index') }}">プラン一覧</a><br>
+<a href="#">お問い合わせ</a>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
 
 Route::get('/dashboard', function () {
@@ -16,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
