53616e12b2e47dafbc1f7df4e8befc67be8bc231
ryota1313 <gbale.jwil.cfab@gmail.com>
2025-03-03 15:46:16 +0900

    検索フォーム作成

---
+++ resources/views/Booking/index.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-slot name="header"><h1>予約一覧</h1></x-slot>
+    <div class="mx-20 my-4">
+            <table>
+                <thead>
+                    <tr>
+                        <th>予約者名</th>
+                        <th>メールアドレス</th>
+                        <th>予約番号</th>
+                    </tr>
+                </thead>
+    
--- resources/views/Booking/search.blade.php
+++ resources/views/Booking/search.blade.php
@@ -1,1 +1,17 @@
-aaa
+<x-app-layout>
+    <x-slot name="header"><h1>予約検索</h1></x-slot>
+    <div class="mx-20 my-4">
+        <form action="{{ route('booking.index') }}" method="get">
+            @csrf
+            <dl>
+                <dt>予約者名</dt>
+                <dd class="mx-4"><input type="text" name="name" placeholder="予約者名"></dd>
+                <dt>メールアドレス</dt>
+                <dd class="mx-4"><input type="text" name="email" placeholder="メールアドレス"></dd>
+                <dt>予約番号</dt>
+                <dd class="mx-4"><input type="text" name="id" placeholder="予約番号"></dd>
+            </dl>
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md my-4">検索</button>
+        </form>
+    </div>
+</x-app-layout>
